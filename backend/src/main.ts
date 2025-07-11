2f92ab7fe9c10ad009a8140d39f84056de41745a
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-07-11 20:37:38 +0530

    updated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,16 +9,16 @@ const PORT = Number(process.env.PORT) || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000', // Specific origin
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     credentials: true,
   });
 
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // remove properties that don't have decorators
-      forbidNonWhitelisted: true, // throw an error for extra fields
-      transform: true, // convert payloads to DTO instances
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
   await app.listen(PORT);
